df4044abc55174e4c75564603134f1b4c236f553
dvidCR <dvidcr100@gmail.com>
2024-10-08 23:30:38 +0200

    9 Commit

--- 1º Trimestre/Ejercicios/login/login.py
+++ 1º Trimestre/Ejercicios/login/login.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLineEdit, QLabel, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLineEdit, QLabel, QGridLayout, QDialog, QHBoxLayout
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon, QPixmap
 
 class Login(QMainWindow):
     
@@ -11,8 +12,11 @@ class Login(QMainWindow):
         self.passw = passw
         
         self.setWindowTitle("Inicio de sesión")
+        
         self.widget = QWidget()
         self.layoutG = QGridLayout()
+        self.layoutH = QHBoxLayout()
+        self.dlg = QDialog(self)
         
     def position(self):
         label = QLabel("Inicio de sesión")
@@ -26,7 +30,6 @@ class Login(QMainWindow):
         
         show = QPushButton("Mostrar")
         show.setCheckable(True)
-        show.setChecked(True)
         show.clicked.connect(self.showPass)
         
         login = QPushButton("Iniciar Sesion")
@@ -46,16 +49,41 @@ class Login(QMainWindow):
         self.window()
         
     def login(self):
+        img_label = QLabel(self)
+
         if self.user == self.inputUser.text() and self.passw == self.inputPassw.text():
+            self.dlg.setWindowTitle("Inicio de sesión correcto")
+            self.dlg.setWindowIcon(QIcon("ok.ico"))
+            
+            img = QPixmap("ok.png")
+            img_label.setPixmap(img)
+            
             label = QLabel("Has iniciado sesión correctamente")
-            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            self.setCentralWidget(label)
+            label.setStyleSheet("QLabel {color: green}")
         else:
             self.inputUser.clear()
             self.inputPassw.clear()
+            
+            self.dlg.setWindowTitle("Inicio de sesión incorrecto")
+            self.dlg.setWindowIcon(QIcon("error.ico"))
+            
+            img = QPixmap("error.png")
+            img_label.setPixmap(img)
+            
             label = QLabel("Usuario o contraseña incorrecto")
-            self.layoutG.addWidget(label, 4, 1)
-            self.window()
+            label.setStyleSheet("QLabel {color: red}")
+
+        img_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        self.layoutH.addWidget(img_label)
+        self.layoutH.addWidget(label)
+        
+        self.dlg.setLayout(self.layoutH)
+        self.dlg.exec()
+        
+        self.layoutH.removeWidget(img_label)
+        self.layoutH.removeWidget(label)
             
     def showPass(self, s):
         if s:

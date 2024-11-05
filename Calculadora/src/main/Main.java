0069e9877b4a8f48e3c22d54c415156b1cff7ca7
dvidCR <dvidcr100@gmail.com>
2024-11-05 18:32:33 +0100

    13 Commit

--- Calculadora/src/main/Main.java
+++ Calculadora/src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import net.objecthunter.exp4j.tokenizer.UnknownFunctionOrVariableException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.Reflection;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -25,16 +29,49 @@ public class Main extends Application {
 		
 		TextField tf = new TextField();
 		
+		VBox layoutV = new VBox(10);
+		HBox layoutH = new HBox(10);
 		GridPane grid = new GridPane();
 		
 		for (int i = 0; i < botones.length; i++) {
 			for (int j = 0; j < botones[i].length; j++) {
 				Button boton = new Button(botones[i][j]);
-				grid.add(boton, i, j);
+				boton.setOnAction(e -> {
+					if(boton.getText().equals("=")) {
+						Expression calc = null;
+						
+						try {
+							calc = (Expression) new ExpressionBuilder(tf.getText()).build();
+							double resultado = calc.evaluate();
+                            tf.setText(String.valueOf(resultado));
+						} catch (UnknownFunctionOrVariableException ex) {
+                            tf.setText("Función desconocida");
+                            ex.printStackTrace();
+                        } catch (Exception ex) {
+                            tf.setText("Error");
+                            ex.printStackTrace();
+                        }
+					} else if (boton.getText().equals("C")) {
+						tf.clear();
+					} else if (boton.getText().equals("->")) {
+						String content = tf.getText();
+						if (content.length() > 0) {
+                            content = content.substring(0, content.length() - 1);
+                            tf.setText(content);
+						}
+					} else {
+						tf.setText(tf.getText() + boton.getText());
+					}
+					
+				});
+				grid.add(boton, j, i+1);
 			}
 		}
 		
-		Scene escena = new Scene(grid, 400, 400);
+		layoutH.getChildren().add(tf);
+		layoutV.getChildren().addAll(layoutH, grid);
+		
+		Scene escena = new Scene(layoutV, 400, 400);
 		Stage.setScene(escena);
 		Stage.setTitle("Calculadora");
 		Stage.show();
